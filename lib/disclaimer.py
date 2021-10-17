1e21b7ffb5a45c0716736af105c66a2a55b38e41
Richard Mwewa <74001397+rlyonheart@users.noreply.github.com>
2021-10-18 00:00:41 +0200

    Create disclaimer.py

---
+++ lib/disclaimer.py
@@ -0,0 +1,7 @@
+from lib.colors import red,white,reset
+
+message = f"""{red}
+Disclaimer{white}:
+This tool was sorely developed for educational purposes only
+and should not be used in environments without {red}legal authorization.{reset}
+"""
