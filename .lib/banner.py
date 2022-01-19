10a89e00121e01eb9cb94dafa46844b9e8c8d846
Richard Mwewa <74001397+rly0nheart@users.noreply.github.com>
2022-01-19 13:27:25 +0200

    Create banner.py

---
+++ .lib/banner.py
@@ -0,0 +1,10 @@
+import os
+import sys
+sys.path.append(os.getcwd()+"/.lib/")
+from colors import red,white,reset
+
+banner = f'''
+{white}___ _  _ ____ ____ _  _ ___  _   _ _  _ ____ 
+ |  |__| |  | |__/ |\ | |  \  \_/  |_/  |___ 
+{red} |  |  | |__| |  \ | \| |__/   |   | \_ |___
+{reset}'''
