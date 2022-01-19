24adc1810767fcc9556c11fbd997f2a873468d6d
Richard Mwewa <74001397+rly0nheart@users.noreply.github.com>
2022-01-19 17:09:22 +0200

    Update default_lookup.py

--- .lib/default_lookup.py
+++ .lib/default_lookup.py
@@ -1,17 +1,12 @@
 import os
 import sys
-sys.path.append(os.getcwd()+"/.lib/")
+sys.path.append(f'{os.getcwd()}/.lib/')
 from colors import red,white,green,reset
 
 # The normal function will return both found and not found results
 # This is called by default if both the  -f/--found & -n/--not-found flags are not specified
-def default(args,response,site_url,site_name,data):
+def default(args,response,site_url,site_name):
     if response.status_code == 200:
-    	print(f'\n{white}{site_name}{reset}')
-    	for key,value in data.items():
-    	    print(f'{white}├─ {key}: {green}{value}{reset}')   
+    	print(f'{white}[{green}+{white}] {site_name}: {green}{site_url.format(args.username)}{reset}')
     else:
-        color = red
-        print(f'\n{white}{site_name}{reset}')
-        for key,value in data.items():
-            print(f'{white}├─ {key}: {red}{value}{reset}')
+    	print(f'{white}[{red}-{white}] {site_name}: {red}Not Found{reset}')
