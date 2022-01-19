f26f549ab8f5ca6ef1bf7c2157bfae4e27fe0c04
Richard Mwewa <74001397+rly0nheart@users.noreply.github.com>
2022-01-19 17:11:56 +0200

    Update not_found.py

--- .lib/not_found.py
+++ .lib/not_found.py
@@ -1,12 +1,10 @@
 import os
 import sys
-sys.path.append(os.getcwd()+"/.lib/")
-from colors import red,white,green,reset
+sys.path.append(f'{os.getcwd()}/.lib/')
+from colors import red,white,reset
 
 # The notfound function returns not found results only
 # And it can be called by specifying the -n/--not-found flags
-def not_found(args,response,site_name,site_url,data):
+def not_found(args,response,site_name,site_url):
     if response.status_code != 200:
-        print(f'\n{white}{site_name}{reset}')
-        for key,value in data.items():
-            print(f'{white}├─ {key}: {red}{value}{reset}')
+    	print(f'{white}[{red}-{white}] {site_name}: {red}Not Found{reset}')
