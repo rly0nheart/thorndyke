482c7d7da512f6665ae4fee15e36ff0483b1aa4b
Richard Mwewa <74001397+rly0nheart@users.noreply.github.com>
2022-01-19 17:10:52 +0200

    Update found.py

--- .lib/found.py
+++ .lib/found.py
@@ -1,12 +1,10 @@
 import os
 import sys
-sys.path.append(os.getcwd()+"/.lib/")
-from colors import red,white,green,reset
+sys.path.append(f'{os.getcwd()}/.lib/')
+from colors import green,white,reset
 
 # The found function returns found results only
 # And it can be called by specifying the -f/--found flags        
-def found(args,response,site_name,site_url,data):
+def found(args,response,site_name,site_url):
     if response.status_code == 200:      
-        print(f'\n{white}{site_name}{reset}')
-        for key,value in data.items():
-        	print(f'{white}├─ {key}: {green}{value}{reset}')
+        print(f'{white}[{green}+{white}] {site_name}: {green}{site_url.format(args.username)}{reset}')
